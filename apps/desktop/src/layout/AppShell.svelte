7e6bcbf6769e072f1b4c5aceaaf929717a931c65
agusnieto77 <agustin.nieto77@gmail.com>
2026-04-18 22:59:12 -0300

    up

--- apps/desktop/src/layout/AppShell.svelte
+++ apps/desktop/src/layout/AppShell.svelte
@@ -27,9 +27,6 @@
     <p>
       Desarrollado por
       <a href={HLAB_URL} onclick={openHlabWebsite}>HLab (Laboratorio de Humanidades Digitales)</a>
-      <a href="https://hlab.com.ar/" target="_blank" rel="noopener noreferrer"
-        >HLab (Laboratorio de Humanidades Digitales)</a
-      >
     </p>
   </footer>
 </div>
